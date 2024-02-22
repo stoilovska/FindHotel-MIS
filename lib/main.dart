467cce31305d2f59b8a1415ba75e27a82722e977
stoilovska <teodora.stoilovska@gmail.com>
2024-02-22 01:24:02 +0100

    Implementing login and register pages with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:find_hotel_mis/firebaseOptions/firebase_options.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
@@ -22,7 +28,13 @@ class MyApp extends StatelessWidget {
         )),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'FindHotel'),
+      // home: const MyHomePage(title: 'FindHotel'),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const MyHomePage(title: 'FindHotel'),
+        '/login': (context) => const AuthScreen(isLogin: true),
+        '/register': (context) => const AuthScreen(isLogin: false),
+      },
     );
   }
 }
@@ -37,11 +49,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
+  void _navigateToLogin() {
+    Future.delayed(Duration.zero, () {
+      Navigator.pushReplacementNamed(context, '/login');
     });
   }
 
@@ -85,7 +96,10 @@ class _MyHomePageState extends State<MyHomePage> {
               height: 60.0,
               margin: const EdgeInsets.only(bottom: 16.0),
               child: FloatingActionButton(
-                onPressed: _incrementCounter,
+                onPressed: () {
+                  // Navigate to the login page
+                  _navigateToLogin();
+                },
                 tooltip: 'Increment',
                 backgroundColor: Colors.grey,
                 child: const Text(
@@ -103,3 +117,139 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+class AuthScreen extends StatefulWidget {
+  final bool isLogin;
+
+  const AuthScreen({super.key, required this.isLogin});
+
+  @override
+  AuthScreenState createState() => AuthScreenState();
+}
+
+class AuthScreenState extends State<AuthScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final GlobalKey<ScaffoldMessengerState> _scaffoldKey =
+  GlobalKey<ScaffoldMessengerState>();
+
+  Future<void> _authAction() async {
+    try {
+      if (widget.isLogin) {
+        await _auth.signInWithEmailAndPassword(
+          email: _emailController.text,
+          password: _passwordController.text,
+        );
+        _showSuccessDialog(
+            "Login Successful",
+            "Welcome back! Your login was a success. "
+                "Explore, engage, and enjoy your time with us.");
+        _navigateToHome();
+      } else {
+        await _auth.createUserWithEmailAndPassword(
+          email: _emailController.text,
+          password: _passwordController.text,
+        );
+        _showSuccessDialog(
+            "Registration Successful",
+            "Success! Your registration is complete. Thanks for joining us. "
+                "Start exploring and make the most of your experience.");
+        _navigateToLogin();
+      }
+    } catch (e) {
+      _showErrorDialog(
+          "Authentication Error", "Error during authentication: $e");
+    }
+  }
+
+  void _showSuccessDialog(String title, String message) {
+    _scaffoldKey.currentState?.showSnackBar(SnackBar(
+      content: Text(message),
+      duration: const Duration(seconds: 2),
+    ));
+  }
+
+  void _showErrorDialog(String title, String message) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("OK"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _navigateToHome() {
+    Future.delayed(Duration.zero, () {
+      Navigator.pushReplacementNamed(context, '/');
+    });
+  }
+
+  void _navigateToLogin() {
+    Future.delayed(Duration.zero, () {
+      Navigator.pushReplacementNamed(context, '/login');
+    });
+  }
+
+  void _navigateToRegister() {
+    Future.delayed(Duration.zero, () {
+      Navigator.pushReplacementNamed(context, '/register');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: widget.isLogin ? const Text("Login") : const Text("Register"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _emailController,
+              decoration: const InputDecoration(labelText: "Email"),
+            ),
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(labelText: "Password"),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _authAction,
+              child: Text(widget.isLogin ? "Sign In" : "Register"),
+            ),
+            if (!widget.isLogin)
+              ElevatedButton(
+                onPressed: _navigateToLogin,
+                child: const Text('Already have an account? Login'),
+              ),
+            if (widget.isLogin)
+              ElevatedButton(
+                onPressed: _navigateToRegister,
+                child: const Text('Create an account'),
+              ),
+            TextButton(
+              onPressed: _navigateToHome,
+              child: const Text('Back to Main Screen'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
